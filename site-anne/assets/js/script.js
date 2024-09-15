38c938948a91227b7f31ee1feb4e99010f1be68f
FelipeFeerreira1 <felipeferreiradosreis0@gmail.com>
2024-09-15 15:30:05 -0300

    Commit 1

---
+++ site-anne/assets/js/script.js
@@ -0,0 +1,128 @@
+'use strict';
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * PRELOADER
+ * 
+ * preloader will be visible until document load
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+
+
+/**
+ * MOBILE NAVBAR
+ * 
+ * show the mobile navbar when click menu button
+ * and hidden after click menu close button or overlay
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+
+
+/**
+ * HEADER & BACK TOP BTN
+ * 
+ * active header & back top btn when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", activeElementOnScroll);
+
+
+
+/**
+ * SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const revealElementOnScroll = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealElementOnScroll);
+
+window.addEventListener("load", revealElementOnScroll);
+
+// navbar toggling
+const navbarShowBtn = document.querySelector('.navbar-show-btn');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+const navbarHideBtn = document.querySelector('.navbar-hide-btn');
+
+navbarShowBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.add('navbar-show');
+});
+navbarHideBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.remove('navbar-show');
+});
+
+// changing search icon image on window resize
+window.addEventListener('resize', changeSearchIcon);
+function changeSearchIcon(){
+    let winSize = window.matchMedia("(min-width: 1200px)");
+    if(winSize.matches){
+        document.querySelector('.search-icon img').src = "images/search-icon.png";
+    } else {
+        document.querySelector('.search-icon img').src = "images/search-icon-dark.png";
+    }
+}
+changeSearchIcon();
+
+// stopping all animation and transition
+let resizeTimer;
+window.addEventListener('resize', () =>{
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
